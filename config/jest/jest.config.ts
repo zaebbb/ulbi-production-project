159a6408c78d487ba5ded231a1383ed76e7a86c5
zaebbb <elik@example.com>
2023-05-16 22:11:21 +0500

    Edit config: jest, eslint, tsconfig, test components: Button, Sidebar, install modules worked test react components, add config test i18next, add utils i18next testing - refs #24

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from "path";
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -42,6 +44,14 @@ export default {
     'clover',
     'html',
   ],
+  modulePaths: [
+    '<rootDir>src',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.s?css$': 'identity-obj-proxy',
+    "\\.svg": path.resolve(__dirname, 'jestEmptyComponent.tsx')
+  },
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
@@ -70,6 +80,7 @@ export default {
   // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: [
     'node_modules',
+    'src'
   ],
 
   // An array of file extensions your modules use
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,23 @@
+import {fireEvent, screen} from '@testing-library/react'
+import { Sidebar } from 'widgets/Sidebar'
+import {
+  renderWithTranslations
+} from "shared/lib/tests/renderWithTranslations/renderWithTranslations";
+describe('UI компонент <Sidebar />', () => {
+  test(
+    'Рендер компонента',
+    () => {
+      renderWithTranslations(<Sidebar />)
+      expect(screen.getByTestId('sidebar')).toBeInTheDocument()
+    })
+
+  test(
+    'Тест на сворачивание сайдбара',
+    () => {
+      renderWithTranslations(<Sidebar />)
+      const toggleBtn = screen.getByTestId('sidebar-toggle')
+      expect(screen.getByTestId('sidebar')).toBeInTheDocument()
+      fireEvent.click(toggleBtn)
+      expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
+    })
+})
