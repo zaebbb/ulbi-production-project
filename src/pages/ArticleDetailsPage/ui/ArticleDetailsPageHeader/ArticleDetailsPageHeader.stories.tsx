ef8b9a8052e4925c418dad498b072c85892c147d
zaebbb <elik@example.com>
2023-07-18 22:50:31 +0500

    Refactor Stories - refs #61

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.stories.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { ArticleDetailsPageHeader } from './ArticleDetailsPageHeader'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 const meta: Meta<typeof ArticleDetailsPageHeader> = {
   title: 'CHANGE/ArticleDetailsPageHeader',
@@ -10,3 +11,6 @@ export default meta
 type Story = StoryObj<typeof ArticleDetailsPageHeader>
 
 export const Primary: Story = {}
+Primary.decorators = [
+  StoreDecorator({}),
+]
--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.stories.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { ArticleEditPage } from './ArticleEditPage'
+import ArticleEditPage from './ArticleEditPage'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 const meta: Meta<typeof ArticleEditPage> = {
   title: 'CHANGE/ArticleEditPage',
@@ -10,3 +11,6 @@ export default meta
 type Story = StoryObj<typeof ArticleEditPage>
 
 export const Primary: Story = {}
+Primary.decorators = [
+  StoreDecorator({}),
+]
