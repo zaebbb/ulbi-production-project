45d625ce4327b256bb73a8eb7a4784eabe83756b
zaebbb <elik@example.com>
2023-07-19 22:01:38 +0500

    Public project - refs #62

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,13 +1,4 @@
 import React from 'react'
 
 export const AboutPageAsync = React
-  .lazy(
-    async () => await new Promise((resolve) => {
-      setTimeout(
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        () => { resolve(import('./AboutPage')) },
-        15000
-      )
-    })
-  )
+  .lazy(async () => await import('./AboutPage'))
