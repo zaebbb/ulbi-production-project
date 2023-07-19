0874cc80d071117606db3953e3dd49dda234a191
zaebbb <elik@example.com>
2023-07-19 22:32:30 +0500

    Fix api errors - refs #63

--- src/shared/api/app.ts
+++ src/shared/api/app.ts
@@ -3,7 +3,10 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/const/localstorage'
 
 export const $api: AxiosInstance = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY),
-  },
+})
+
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || ''
+
+  return config
 })
