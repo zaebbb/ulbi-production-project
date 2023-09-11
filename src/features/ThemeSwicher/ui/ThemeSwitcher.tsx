0b9a6bc6f89843c5f37fafcbdc6a96e5a7d9c271
zaebbb <elik@example.com>
2023-09-11 20:23:47 +0500

    Fix svg sizes and color - refs #132

--- src/features/ThemeSwicher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwicher/ui/ThemeSwitcher.tsx
@@ -1,12 +1,11 @@
 import React, { memo } from 'react'
 import { saveJsonSettings } from '@/entities/User'
-import DarkIcon from '@/shared/assets/icons/theme-dark.svg'
-import LightIcon from '@/shared/assets/icons/theme-light.svg'
-import { Theme } from '@/shared/const'
+import ThemeIcon from '@/shared/assets/icons/theme-dark.svg'
 import { classNames } from '@/shared/lib/classNames/classNames'
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch'
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme'
 import { Button, ThemeButton } from '@/shared/ui/Button'
+import { Icon } from '@/shared/ui/Icon'
 
 interface ThemeSwitcherProps {
   className?: string
@@ -14,7 +13,7 @@ interface ThemeSwitcherProps {
 
 export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = memo((props: ThemeSwitcherProps) => {
   const { className } = props
-  const { toggleTheme, theme } = useTheme()
+  const { toggleTheme } = useTheme()
   const dispatch = useAppDispatch()
 
   const onToggleTheme = React.useCallback(() => {
@@ -30,9 +29,7 @@ export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = memo((props: ThemeSwi
       theme={ThemeButton.CLEAR}
 
     >
-      {
-        theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />
-      }
+      <Icon Svg={ThemeIcon} width={40} height={40} isInverted />
     </Button>
   )
 })
