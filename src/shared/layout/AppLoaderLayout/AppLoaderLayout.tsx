08610cc7420de26be2f75e578d8da07ec08a0b48
zaebbb <elik@example.com>
2023-09-28 21:46:34 +0500

    Add AppLoaderLayout, fallback theme and used design - refs #150

---
+++ src/shared/layout/AppLoaderLayout/AppLoaderLayout.tsx
@@ -0,0 +1,27 @@
+import React, { memo } from 'react'
+import cls from './AppLoaderLayout.module.scss'
+import { MainLayout } from '@/shared/layout/MainLayout'
+import { Skeleton } from '@/shared/ui/redesigned/Skeleton'
+import { HStack, VStack } from '@/shared/ui/redesigned/Stack'
+
+export const AppLoaderLayout: React.FC =
+  memo(() => (
+    <MainLayout
+      header={
+        <HStack className={cls.header}>
+          <Skeleton width={40} height={40} border={'50%'} />
+        </HStack>
+      }
+      content={
+        <VStack gap={16} className={cls.content}>
+          <Skeleton width={'70%'} height={32} border={'16px'} />
+          <Skeleton width={'40%'} height={20} border={'16px'} />
+          <Skeleton width={'50%'} height={20} border={'16px'} />
+          <Skeleton width={'30%'} height={32} border={'16px'} />
+          <Skeleton width={'80%'} height={'40%'} border={'16px'} />
+          <Skeleton width={'80%'} height={'40%'} border={'16px'} />
+        </VStack>
+      }
+      sidebar={<Skeleton border={'32px'} width={220} height={'100%'} />}
+    />
+  ))
--- src/shared/lib/features/lib/setGetFeatures.ts
+++ src/shared/lib/features/lib/setGetFeatures.ts
@@ -1,6 +1,13 @@
 import { type FeatureFlags } from '../../../types/featuresFlags'
+import { LOCAL_STORAGE_LAST_DESIGN_KEY } from '@/shared/const/localstorage'
 
-let featuresFlags: FeatureFlags = {}
+const defaultFeatures = {
+  isAppRedesigned: localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY) === 'new',
+}
+
+let featuresFlags: FeatureFlags = {
+  ...defaultFeatures,
+}
 
 export const setFeatureFlags = (newFeatureFlags?: FeatureFlags) => {
   if (newFeatureFlags) {
